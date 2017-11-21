643f2284fe90088c465c4c3d7375987eb98d4568
Nicolas Luco <nico_luco@hotmail.com>
2017-11-21 10:57:05 +0100

    making functions for better code

---
+++ data.js
@@ -0,0 +1,26 @@
+let movieData = {
+    info: 'A verry good movie about a lovley Alien',
+    title: 'E.T.',
+    actor: ['ET','Boy with game issues','Gangster Goverment']
+};
+
+movieTitleWithDescr();
+actorList();
+
+
+
+
+//==============Functions========================================
+function movieTitleWithDescr() {
+    document.getElementById("descText").innerText = movieData.info;
+    document.getElementById("title").innerText = movieData.title;
+}
+
+function actorList() {
+    let tempActor = "";
+    movieData.actor.forEach(element => {
+        tempActor += "<li>" + element + "</li>";
+    });
+    document.getElementById("actorList").innerHTML = tempActor;
+}
+
